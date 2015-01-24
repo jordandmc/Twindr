dab241d09d901f630c01fec8b38a51ffe1a9928b
Evan Spearman <evanspearman@gmail.com>
2015-01-23 22:31:39 -0600

    Added Empty iOS Swift Project.

---
+++ iOS/Twindr/Twindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twindr
+//
+//  Created by Evan Spearman on 2015-01-23.
+//  Copyright (c) 2015 Group1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
