55a32cd158d0fadf175b72ab9deea604c0dac3fe
Morgan Epp <morganepp@mepp-2.local>
2015-02-02 15:35:59 -0600

    Basic app mockup, still needs some overall organization, status bar text isn't white yet.

--- iOS/Twindr/Twindr/ViewController.swift
+++ iOS/Twindr/Twindr/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 39.0/255.0, green: 174.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
     }
 
     override func didReceiveMemoryWarning() {
