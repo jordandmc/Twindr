3b7178bf9dfc8c5778f75de4d13f208bc8d52fc1
Tim <steeltim@gmail.com>
2015-02-14 14:36:47 -0600

    Updated profile settings page. Added the web form to update registration fields. Refactored registration form.

--- app/controllers/Profile.scala
+++ app/controllers/Profile.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import business.domain.Registration
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc.Controller
 
 /**
@@ -7,8 +10,20 @@ import play.api.mvc.Controller
  */
 object Profile extends Controller {
 
+  val registrationForm = Form(
+    mapping (
+      "sex" -> text.verifying("Please enter your sex", {!_.isEmpty}),
+      "birthday" -> date("yyyy-mm-dd").verifying("Your birth date must be in the past", {_.before(new java.util.Date())}),
+      "interests" -> text
+    )(Registration.apply)(Registration.unapply)
+  )
+
   def settings = AuthAction { implicit request =>
-    Ok(views.html.settings(request))
+    Ok(views.html.settings(registrationForm)(request))
+  }
+
+  def filters = AuthAction { implicit request =>
+    Ok(views.html.filters(request))
   }
 
 }
