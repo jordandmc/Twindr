716aa6e04a498dacd55a85576bf87b7529652fe1
Morgan Epp <mrgnepp@gmail.com>
2015-03-02 02:21:31 -0600

    Created matches screen

--- iOS/Twindr/Twindr/MatchesViewController.swift
+++ iOS/Twindr/Twindr/MatchesViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class MatchesViewController: ViewController {
     
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var tweet1Label: UILabel!
+    @IBOutlet weak var tweet2Label: UILabel!
+    @IBOutlet weak var tweet3Label: UILabel!
+    @IBOutlet weak var tweet4Label: UILabel!
+    @IBOutlet weak var tweet5Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.setToolbarHidden(false, animated: false)
+        self.navigationController?.toolbar.barTintColor = UIColor(red: CGFloat(39.0/255.0), green: CGFloat(174.0/255.0), blue: CGFloat(96.0/255.0), alpha: CGFloat(1.0))
+        self.navigationController?.toolbar.tintColor = UIColor.whiteColor()
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +30,32 @@ class MatchesViewController: ViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Decline to be matched with the user via button or swipe
+    func dismissMatch() {
+
+    }
+    
+    // Accept the match via button or swipe
+    func acceptMatch() {
+        
+    }
+    
+    @IBAction func dismissMatch(sender: UIBarButtonItem) {
+        dismissMatch()
+    }
+    
+    // Swipe to the right (start left, move right)
+    @IBAction func dismissMatch(sender: UISwipeGestureRecognizer) {
+        dismissMatch()
+    }
+    
+    @IBAction func acceptMatch(sender: UIBarButtonItem) {
+        acceptMatch()
+    }
+    
+    // Swipe to the left (start right, move left)
+    @IBAction func acceptMatch(sender: UISwipeGestureRecognizer) {
+        acceptMatch()
+    }
     
 }
