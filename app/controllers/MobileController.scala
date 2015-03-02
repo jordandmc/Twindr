acee034a54b7e7f2229d7652013ec7d0e15f3f89
junhyeok <visual713@gmail.com>
2015-03-01 22:17:39 -0600

    more mobile access point

--- app/controllers/MobileController.scala
+++ app/controllers/MobileController.scala
@@ -1,7 +1,8 @@
 package controllers
 
+import business.domain.PotentialMatchResponse
 import business.logic.MatchingManager
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, JsSuccess, Json}
 import play.api.mvc.Controller
 
 object MobileController extends Controller {
@@ -11,9 +12,28 @@ object MobileController extends Controller {
     Ok(Json.toJson(potentialMatches))
   }
 
+  def processMatchResponse = AuthAction { request =>
+    request.body.asJson match {
+      case Some(js: JsValue) =>
+        Json.fromJson[PotentialMatchResponse](js) match {
+          case response: JsSuccess[PotentialMatchResponse] =>
+            MatchingManager.processMatchResponse(request.user, response.get)
+            Ok
+          case _ =>
+            BadRequest("Invalid JSON")
+        }
+      case _ =>
+        BadRequest("Invalid Response")
+    }
+  }
+
   def getMatches = AuthAction { request =>
     val matches = MatchingManager.getMatches(request.user)
     Ok(Json.toJson(matches))
   }
 
+  def unmatch = AuthAction { request =>
+    MatchingManager.unmatch(request.user,request.body.asText.getOrElse(""))
+    Ok
+  }
 }
