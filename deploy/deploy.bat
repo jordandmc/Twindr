d25dc7f755747fafbf6423828ae5e073bd42ef81
Jordan Cole <jordandmc@gmail.com>
2015-03-02 04:02:19 -0600

    - removed some commented out code - added an argument to the command that starts the web server to use the prod config

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -6,8 +6,6 @@ start "Compile Twindr" "CMD /C sbt dist"
 echo Continue when compiling is done.
 pause
 cd deploy
-::timeout 20
-goto comment
 echo Uploading zip to the server machine...
 pscp -i ./TwindrSSH.ppk ../target/universal/twindr-1.0.zip ec2-user@54.149.24.39:./Downloads
 if %errorlevel% EQU 1 echo There was an error uploading zip! && pause && exit 1
@@ -15,7 +13,6 @@ echo Unziping the files on the server...
 plink -ssh -i ./TwindrSSH.ppk ec2-user@54.149.24.39 unzip -o ./Downloads/twindr-1.0.zip
 echo Restarting the web server...
 plink -ssh -t -i ./TwindrSSH.ppk ec2-user@54.149.24.39 sudo kill -15 `sudo lsof -t -i:80` &
-plink -ssh -t -i ./TwindrSSH.ppk ec2-user@54.149.24.39 sudo twindr-1.0/bin/twindr -Dhttp.port=80 &
-:comment
+plink -ssh -t -i ./TwindrSSH.ppk ec2-user@54.149.24.39 sudo twindr-1.0/bin/twindr -Dconfig.resource=production.conf -Dhttp.port=80 &
 echo Done deploying to server.
 pause
