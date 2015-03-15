d76b831211577b14bbb369ab87deeaead0aebfcb
Jordan <Jordan@Jordans-Mac.local>
2015-03-15 18:01:17 -0500

    removed example code

--- iOS/Twindr/TwindrTests/TwindrTests.swift
+++ iOS/Twindr/TwindrTests/TwindrTests.swift
@@ -28,16 +28,4 @@ class TwindrTests: XCTestCase {
         XCTAssertNil(Twitter.sharedInstance().session(), "Error: logging out")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
