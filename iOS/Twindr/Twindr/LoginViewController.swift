001d1a869334a8477d2264b250c426d1f23a5fd1
Jordan <Jordan@Jordans-Mac.local>
2015-03-16 20:34:42 -0500

    refactored code into a helper class

--- iOS/Twindr/Twindr/LoginViewController.swift
+++ iOS/Twindr/Twindr/LoginViewController.swift
@@ -38,57 +38,11 @@ class LoginViewController: ViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    private func postOauthCredentials(session:TWTRSession) -> Bool {
-        var token = session.authToken
-        var secret = session.authTokenSecret
-        var result = post(["authToken":token, "authTokenSecret":secret],"http://localhost:/login")
+    func postOauthCredentials(session:TWTRSession) -> Bool {
+        let helper = NetworkHelper()
+        var host:String = helper.getPlistKey("TwindrURL")
         
-        if(!result){
-            println("There was a problem communicating with the server.")
-        }
-        
+        return helper.post(["authToken":session.authToken, "authTokenSecret":session.authTokenSecret], url:  host + "m/login")
     }
     
-    func post(params : Dictionary<String, String>, url : String) -> Bool {
-        var result = false
-        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        var session = NSURLSession.sharedSession()
-        var err: NSError?
-        
-        request.HTTPMethod = "POST"
-        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            println("Response: \(response)")
-            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println("Body: \(strData)")
-            var err: NSError?
-            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
-            
-            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-            if(err != nil) {
-                println(err!.localizedDescription)
-                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                println("Error could not parse JSON: '\(jsonStr)'")
-            }
-            else {
-                // The JSONObjectWithData constructor didn't return an error. But, we should still
-                // check and make sure that json has a value using optional binding.
-                if let parseJSON = json {
-                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-                    var success = parseJSON["success"] as? Int
-                    println("Succes: \(success)")
-                }
-                else {
-                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                    println("Error could not parse JSON: \(jsonStr)")
-                }
-            }
-        })
-        task.resume()
-        return result
-    }
 }
