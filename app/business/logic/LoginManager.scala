c714dce129d233a66d1b4efeaedcadbff0841866
Jordan Cole <jordandmc@gmail.com>
2015-03-23 03:52:23 -0500

    added a mobileLogin method for clients that don't have oauth tokens

--- app/business/logic/LoginManager.scala
+++ app/business/logic/LoginManager.scala
@@ -1,10 +1,9 @@
 package business.logic
 
 import java.util.UUID
-import com.mongodb.casbah.commons.MongoDBObject
-import controllers.TwitterProvider
-import persistence.DBManager._
+
 import business.domain.{Token, User}
+import controllers.TwitterProvider
 import play.api.libs.oauth.RequestToken
 
 import scala.util.Random
@@ -19,6 +18,28 @@ object LoginManager {
   def login(oauthToken: RequestToken): Token = {
     //TODO find a better way to handle the case when getTwitterName returns None
     val twitterName = TwitterProvider.getTwitterName(oauthToken).getOrElse("@" + UUID.randomUUID().toString)
+    login(twitterName, oauthToken)
+  }
+
+  /**
+   * Execute login logic for mobile login
+   * @param twitterName the screen name of the Twitter user
+   * @return a new authentication token for authenticated web-services and pages
+   */
+  def mobileLogin(twitterName: String): Token ={
+    login(twitterName, null)
+  }
+
+  def logout(token: Token): Unit = {
+    token.delete()
+  }
+
+  /**
+   * Execute login logic
+   * @param twitterName the screen name of the Twitter user
+   * @return a new authentication token for authenticated web-services and pages
+   */
+  private def login(twitterName: String, oauthToken: RequestToken): Token ={
     val user = User.getByTwitterName(twitterName) match {
       case Some(u: User) => u
       case None =>
@@ -26,9 +47,5 @@ object LoginManager {
     }
 
     Token(UUID.randomUUID().toString, user._id).save()
-  }
-
-  def logout(token: Token): Unit = {
-    token.delete()
   }
 }
