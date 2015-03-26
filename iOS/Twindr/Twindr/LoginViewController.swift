487dfc5f8c75739e2eb3ed9c81705baab7ad5abb
jordandmc <jordandmc@gmail.com>
2015-03-26 04:28:17 -0500

    added logic for sending a login request to the server

--- iOS/Twindr/Twindr/LoginViewController.swift
+++ iOS/Twindr/Twindr/LoginViewController.swift
@@ -25,35 +25,36 @@ class LoginViewController: ViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func postOauthCredentials(session:TWTRSession) -> Bool {
-        var host:String = helper.getPlistKey("TwindrURL")
-        
-        return helper.post(["authToken":session.authToken, "authTokenSecret":session.authTokenSecret], url:  host + "m/login")
-    }
-    
     func loginDelegate(session: TWTRSession!, error: NSError!){
         if (session != nil) {
             user = session.userName
-            oauthEcho()
+            println("authtoken: \(session.authToken)")
+            println("authTokenSecret: \(session.authTokenSecret)")
+            sendLogin(session)
         }
         else {
             println("error: \(error.localizedDescription)")
         }
     }
     
-    func oauthEcho(){
-        let oauthSigning = TWTROAuthSigning(
-            authConfig: Twitter.sharedInstance().authConfig,
-            authSession: Twitter.sharedInstance().session())
-        let authHeaders = oauthSigning.OAuthEchoHeadersToVerifyCredentials()
-        let req = NSMutableURLRequest(URL: NSURL(string: "http://192.168.0.107:9000/m/verify_credentials")!)
-        req.allHTTPHeaderFields = authHeaders
+    func sendLogin(session: TWTRSession){
+        var host:String = "http://192.168.0.107:9000/"
+        //var host:String = helper.getPlistKey("TwindrURL")
+        
+        let accessToken:RequestToken = RequestToken(token: session.authToken, secret: session.authTokenSecret)
+        let req = NSMutableURLRequest(URL: NSURL(string: host + "m/login")!)
+        
+        req.HTTPMethod = "POST"
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        req.HTTPBody = accessToken.toJson().dataUsingEncoding(NSUTF8StringEncoding)
         request(req).response(responseHandler)
     }
     
     private func responseHandler(request: NSURLRequest, response: NSHTTPURLResponse?, responseObj: AnyObject?, error: NSError?){
         if(response != nil){
-            if(response!.statusCode == 200){
+            if(response!.statusCode == 200 && responseObj != nil){
+                
+                //JSON.rawData(responseObj?)
                 let navigationController = self.storyboard?.instantiateViewControllerWithIdentifier("StartNav") as UINavigationController
                 self.presentViewController(navigationController, animated: true, completion: nil)
             }
