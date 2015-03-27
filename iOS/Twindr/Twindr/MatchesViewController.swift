3ddc67c4fe0c8b407e03adeae8d9dac50763d7d3
Tim <steeltim@gmail.com>
2015-03-27 14:22:23 -0700

    Hooked up potential matches.

--- iOS/Twindr/Twindr/MatchesViewController.swift
+++ iOS/Twindr/Twindr/MatchesViewController.swift
@@ -17,12 +17,16 @@ class MatchesViewController: ViewController {
     @IBOutlet weak var tweet4Label: UILabel!
     @IBOutlet weak var tweet5Label: UILabel!
     
+    var potentialList: [PreparedPotentialMatch] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationController?.setToolbarHidden(false, animated: false)
         self.navigationController?.toolbar.barTintColor = UIColor(red: CGFloat(39.0/255.0), green: CGFloat(174.0/255.0), blue: CGFloat(96.0/255.0), alpha: CGFloat(1.0))
         self.navigationController?.toolbar.tintColor = UIColor.whiteColor()
+        
+        loadPotentialMatch()
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -36,14 +40,49 @@ class MatchesViewController: ViewController {
     
     // Decline to be matched with the user via button or swipe
     func dismissMatch() {
+        // send that you rejected them
+        Curried().reject(token: xAuthToken!, username: potentialList[0].username)
+        
         // load next potential match
+        potentialList.removeAtIndex(0)
+        loadPotentialMatch()
     }
     
     // Accept the match via button or swipe
     func acceptMatch() {
         // follow that user
         // send that you matched with them
+        Curried().accept(token: xAuthToken!, username: potentialList[0].username)
+        
         // load next potential match
+        potentialList.removeAtIndex(0)
+        loadPotentialMatch()
+    }
+    
+    private func loadPotentialMatch() {
+        if potentialList.count == 0 {
+            potentialList = Curried().getPotentialMatches(token: xAuthToken!)!
+        }
+        
+        if(potentialList.count > 0) {
+            let potentialMatch = potentialList[0]
+            userLabel.text = potentialMatch.username
+            
+            if potentialMatch.tweets.count >= 1 { tweet1Label.text = potentialMatch.tweets[0] } else { tweet1Label.text = "" }
+            if potentialMatch.tweets.count >= 2 { tweet2Label.text = potentialMatch.tweets[1] } else { tweet2Label.text = "" }
+            if potentialMatch.tweets.count >= 3 { tweet3Label.text = potentialMatch.tweets[2] } else { tweet3Label.text = "" }
+            if potentialMatch.tweets.count >= 4 { tweet4Label.text = potentialMatch.tweets[3] } else { tweet4Label.text = "" }
+            if potentialMatch.tweets.count >= 5 { tweet5Label.text = potentialMatch.tweets[4] } else { tweet5Label.text = "" }
+            
+        } else {
+            userLabel.text   = ""
+            tweet1Label.text = ""
+            tweet2Label.text = "We have no more potential matches at this time."
+            tweet3Label.text = ""
+            tweet4Label.text = ""
+            tweet5Label.text = ""
+            
+        }
     }
     
     @IBAction func noButton(sender: UIBarButtonItem) {
